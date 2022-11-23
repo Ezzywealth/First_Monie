1d526db378aa81579dc61f210c0de06b52556362
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2022-11-23 17:02:08 +0100

    db disconnect

---
+++ pages/api/transactions/fetchUser.js
@@ -0,0 +1,19 @@
+import User from "../../../components/Models/User";
+import db from "../../../utils/db";
+
+const handler = async (req, res) => {
+  if (req.method !== "POST") {
+    return res.status(401).send({ message: "Unauthorized method" });
+  }
+
+  const { id } = req.body;
+  try {
+    await db.connect();
+    const data = await User.find({ _id: id }).lean();
+    await db.disconnect();
+    res.status(201).send({ data });
+  } catch (error) {
+    res.status(401).send({ message: "There was an error, please try again" });
+  }
+};
+export default handler;
--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -196,8 +196,8 @@ const Dashboard = ({ newUser }) => {
                   src={session?.user.image}
                   height={100}
                   width={100}
-                  layout='responsive'
                   alt='profile pic'
+                  Layout='responsive'
                   className='w-full'
                 />
               </div>
@@ -408,8 +408,8 @@ export default Dashboard;
 export async function getServerSideProps(ctx) {
   await db.connect();
   const session = await getSession({ ctx });
-  await db.disconnect();
   const currentUser = await User.find({ _id: session?.user._id });
+  await db.disconnect();
 
   const newUser = [
     {
